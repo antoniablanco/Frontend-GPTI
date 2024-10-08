4750a0218370e48dc0baec28ba00888a8eb766a7
Pablo Rabelo <perabelo@uc.cl>
2024-10-08 15:04:49 -0300

    End of setup

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="h-screen flex items-center justify-center bg-blue-100">
       <h1 className="text-4xl font-bold text-blue-600">
-        Hello, Tailwind CSS in React!
+        DestinaAI
       </h1>
     </div>
   );
