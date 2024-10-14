4c63a9ebf3aa45d5ad069320bdc1310ca89f6a60
Pablo Rabelo <perabelo@uc.cl>
2024-10-14 20:18:55 -0300

    Firt Setup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import HomePage from './pages/HomePage';
 import PreferencePage from './pages/PreferencePage';
+import RecomendationsPage from "./pages/RecomendationsPage";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/preferences" element={<PreferencePage />} />
+          <Route path="/recomendations" element={<RecomendationsPage />} />
         </Routes>
       </Router>
     </SesionProvider>
---
+++ src/pages/RecomendationsPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "../components/shared/Navbar";
+
+const RecomendationsPage = () => {
+  return (
+    <>
+      <Navbar />
+
+      <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+        <h1 className="text-5xl font-bold text-gray-800 mb-6 text-center">
+          ¡Aquí estan tus recomendaciones!
+        </h1>
+      </div>
+    </>
+  );
+};
+
+export default RecomendationsPage;
